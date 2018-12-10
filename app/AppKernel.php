5fb53a3b76fb68d05d729c2636f21043916e04c3
simonpiedrahita <simonpiedrahita@gmail.com>
2018-12-10 13:28:27 -0500

    se hace commit para hacer rollback al commit c19b1f37e0c5a74e69ef8f5ebed15edb08cc1a91 y verificar que es lo que sucede

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,10 +17,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
-            new FOS\UserBundle\FOSUserBundle(),
+           new FOS\UserBundle\FOSUserBundle(),
 
             new AppBundle\AppBundle(),
-            
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
